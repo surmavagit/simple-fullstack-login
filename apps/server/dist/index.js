cdd03cd3f402d20210c00ba91d0dfd09be0a107c
George Surmava <111701072+surmavagit@users.noreply.github.com>
2023-04-26 17:24:23 +0200

    Cookie-session middleware testing
    Co-authored-by: denigogov <denigogov@users.noreply.github.com>
    Co-authored-by: Orwa Diraneyya <diraneyya@users.noreply.github.com>

--- apps/server/dist/index.js
+++ apps/server/dist/index.js
@@ -7,15 +7,34 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
+const cookie_session_1 = __importDefault(require("cookie-session"));
 dotenv_1.default.config();
-;
 const app = (0, express_1.default)();
 const port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 4000;
+// TODO: implement more security by only allowing http://localhost:3000
 app.use((0, cors_1.default)());
 const login = {
     loggedin: false,
 };
+;
+// app.set('trust proxy', 1) // trust first proxy
+app.use((0, cookie_session_1.default)({
+    name: 'session',
+    secret: "password123",
+    signed: false
+}));
+app.get('/', function (req, res, next) {
+    var _a;
+    // Set session to a random number
+    if (!((_a = req.session) === null || _a === void 0 ? void 0 : _a.id))
+        req.session = {
+            id: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+        };
+    next();
+});
 app.get("/", (req, res) => {
+    var _a;
+    console.log(`The session object id is ${(_a = req.session) === null || _a === void 0 ? void 0 : _a.id}`);
     res.status(200).json(login);
 });
 app.post("/logout", (req, res) => {
@@ -27,5 +46,5 @@ app.post("/login", (req, res) => {
     res.sendStatus(200);
 });
 app.listen(port, () => {
-    console.log(`⚡️ Server is jumping at http://localhost:${port} `);
+    console.log(`⚡️ Server is running at http://localhost:${port} `);
 });
